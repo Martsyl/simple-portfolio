0c494d69eba54bcd0e2ee9398c30c3c401ef6fb0
Martsyl <msnonuorah3964@gmail.com>
2023-12-09 14:42:28 +0100

    done with a simple portfolio landing page

---
+++ index.js
@@ -0,0 +1,8 @@
+let side = document.getElementById('side_container')
+console.log(side)
+function get_sidepanel() {
+  side.style.display = 'block'
+}
+function close_sidepanel() {
+  side.style.display = 'none'
+}
